accb152c103c786732c6783423bd7bf1c2595f5e
huachao2017 <21006735@qq.com>
2019-07-10 17:48:14 +0800

    - 修订camera使用的序号

--- demo.py
+++ demo.py
@@ -43,7 +43,8 @@ def main(yolo,args):
     writeVideo_flag = True
     
     # video_capture = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)24/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
-    video_capture = cv2.VideoCapture(1)
+    # video_capture = cv2.VideoCapture(1)
+    video_capture = cv2.VideoCapture(0)
 
     if writeVideo_flag:
     # Define the codec and create VideoWriter object
